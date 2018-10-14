80fda8028b1e28f7c7c8071b2674103a0dab590a
pavel_lebedev <designer.lebedev@gmail.com>
2018-10-14 11:13:12 +0300

    Training program

---
+++ precache-manifest.9033418bab2ac3a7360198b7ed35ad1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a05ea3d232a547511bf",
+    "url": "/DAN_IT_programm/static/js/runtime~main.5a05ea3d.js"
+  },
+  {
+    "revision": "816ef3a49920f1040a17",
+    "url": "/DAN_IT_programm/static/js/main.816ef3a4.chunk.js"
+  },
+  {
+    "revision": "6a7976ecf25e0c1ea275",
+    "url": "/DAN_IT_programm/static/js/1.6a7976ec.chunk.js"
+  },
+  {
+    "revision": "816ef3a49920f1040a17",
+    "url": "/DAN_IT_programm/static/css/main.16653efd.chunk.css"
+  },
+  {
+    "revision": "cac6ce5d3c1d6b425b195fb0f25d4137",
+    "url": "/DAN_IT_programm/index.html"
+  }
+];
